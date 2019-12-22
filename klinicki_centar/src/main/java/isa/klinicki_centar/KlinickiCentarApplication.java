52b691c7057cb4070192fbb1e3adb995fdaae685
BobanPozz <bobanpoznanovic1@gmail.com>
2019-12-22 14:28:50 +0100

    Adding project

---
+++ klinicki_centar/src/main/java/isa/klinicki_centar/KlinickiCentarApplication.java
@@ -0,0 +1,13 @@
+package isa.klinicki_centar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlinickiCentarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlinickiCentarApplication.class, args);
+	}
+
+}
