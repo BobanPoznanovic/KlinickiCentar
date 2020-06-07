6d9aed03fcbaef1efe8a54d70fdcc2bf988e9f5a
BobanPozz <bobanpoznanovic1@gmail.com>
2020-06-07 21:08:37 +0200

    Odbijanje popravljeno, potrebno testirati slanje mejla

--- klinicki_centar/src/main/java/isa/klinicki_centar/repositories/ZahtevZaRegistracijuRepository.java
+++ klinicki_centar/src/main/java/isa/klinicki_centar/repositories/ZahtevZaRegistracijuRepository.java
@@ -1,9 +1,15 @@
 package isa.klinicki_centar.repositories;
 
+import java.util.ArrayList;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import isa.klinicki_centar.model.ZahtevZaRegistraciju;
 
 public interface ZahtevZaRegistracijuRepository extends CrudRepository<ZahtevZaRegistraciju, Integer>{
+	
+	@Query(value = "SELECT * FROM zahtev_za_registraciju z WHERE z.status_zahteva = ?1", nativeQuery = true)
+	ArrayList<ZahtevZaRegistraciju> findNonProcessed(String status);
 
 }
--- klinicki_centar/src/main/resources/data.sql
+++ klinicki_centar/src/main/resources/data.sql
@@ -15,7 +15,7 @@ insert into klinika (naziv, adresa, grad, opis, prosecna_ocena, broj_ocena) valu
 insert into klinika (naziv, adresa, grad, opis, prosecna_ocena, broj_ocena) values ('Klinika5', 'Savska 155', 'Beograd', 'Opis klinike5', 0.0, 0);
 
 --ZahtevZaRegistraciju
-insert into zahtev_za_registraciju (broj_osiguranika, email, password, ime, prezime, adresa, grad, drzava, broj_telefona, status_zahteva, razlog_odbijanja) values (1234, 'pera@gmail.com', 'perapera', 'Pera', 'Peric', 'Fruskogorska 1', 'Novi Sad', 'Srbija', '0601234', 1, '');
+insert into zahtev_za_registraciju (broj_osiguranika, email, password, ime, prezime, adresa, grad, drzava, broj_telefona, status_zahteva, razlog_odbijanja) values (1234, 'bobanpoznanovic1@gmail.com', 'perapera', 'Pera', 'Peric', 'Fruskogorska 1', 'Novi Sad', 'Srbija', '0601234', 1, '');
 insert into zahtev_za_registraciju (broj_osiguranika, email, password, ime, prezime, adresa, grad, drzava, broj_telefona, status_zahteva, razlog_odbijanja) values (3214, 'joka@gmail.com', 'jokajoka', 'Joka', 'Jokic', 'Temerinska 1', 'Novi Sad', 'Srbija', '060445566', 1, '');
 
 --Pacijent
